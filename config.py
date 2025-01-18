8702be64938da0ad904b9ac95eb6b479f6ab0d52
Vizrussian <9rikan@mail.ru>
2025-01-18 17:24:36 +0300

    finnaly done

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config(object):
-    DEBUG = True
-    SECRET_HERE = '249y823r9v8238r9u'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(os.path.dirname(__file__), 'movies.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
